e994a5b2eb35d57ff54ebf433bc9f3dd4173f8cf
Mikael Thollesson <mikael.thollesson@ebc.uu.se>
2018-08-31 10:01:47 +0000

    Cleanup

--- Luntan-client/build/temp/development/Luntan/sass/config.rb
+++
@@ -1,2 +0,0 @@
-Compass.add_project_configuration('../../../../../sass/config.rb')
-cache_path = '/Users/mikael/Documents/Programmering/Luntan/Luntan-client/build/.sass-cache'
--- Luntan-client/build/temp/development/Luntan/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,2 +0,0 @@
-// @tag full-page
-// @require /Users/mikael/Documents/Programmering/Luntan/Luntan-client/app.js
