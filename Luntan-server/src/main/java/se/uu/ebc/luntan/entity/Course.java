6b25b16f70a33372345b98a367151ade3107457e
Mikael Thollesson <mikael.thollesson@ebc.uu.se>
2022-06-17 18:21:36 +0200

    Fixed forgotten property copying

--- Luntan-server/src/main/java/se/uu/ebc/luntan/entity/Course.java
+++ Luntan-server/src/main/java/se/uu/ebc/luntan/entity/Course.java
@@ -43,69 +43,69 @@ public class Course  extends Auditable {
 	@GenericGenerator(name = "native", strategy = "native")
     @Column(name = "ID")
     private Long id;
-    
+
     public Long getId() {
         return this.id;
     }
-    
+
     public void setId(Long id) {
         this.id = id;
     }
 
-        
+
     @Column(name = "CODE", length = 255)
     @NotNull
     private String code;
-    
+
     @Column(name = "SE_NAME", length = 255)
     @NotNull
     private String seName;
-    
+
     @Column(name = "COURSE_GROUP", length = 255)
-	@Enumerated(EnumType.STRING)    
+	@Enumerated(EnumType.STRING)
     private CourseGroup courseGroup;
-    
+
     @Column(name = "NOTE", length = 255)
     private String note;
-    
+
     @Column(name = "CREDITS", precision = 12)
     @NotNull
     private Float credits;
 
     @Column(name = "BOARD", length = 255)
-	@Enumerated(EnumType.STRING)    
+	@Enumerated(EnumType.STRING)
     private EduBoard board;
-    
+
     @Column(name = "INACTIVE")
     private boolean inactive = false;
 
-/* 
+/*
     @Column(name = "last_date")
     protected Date lastGivendDate;
  */
 
-/* 
+/*
     @OneToMany(mappedBy = "course")
     @OrderBy("rank ASC")
     private List<Examiner> examiners = new ArrayList<Examiner>();
 
     @OneToMany(mappedBy = "course")
     private Set<CourseInstance> courseInstances = new HashSet<CourseInstance>();
- 
-*/ 
+
+*/
 
  	/* Constructors */
- 	
-/* 
+
+/*
  	public Course() {}
- 	
+
  	public Course (String code, String seName, Float credits, String note) {
  		this.code = code;
  		this.seName = seName;
  		this.credits = credits;
  		this.note = note;
  	}
-     
+
  */
  	public Course (String code, String seName, Float credits, String note, EduBoard board) {
  		this.code = code;
@@ -117,73 +117,74 @@ public class Course  extends Auditable {
 
 
 	/* Setters and getters */
- 
-/* 
+
+/*
     public Set<CourseInstance> getCourseInstances() {
         return courseInstances;
     }
-    
+
     public void setCourseInstances(Set<CourseInstance> courseInstances) {
         this.courseInstances = courseInstances;
     }
-    
+
     public String getCode() {
         return code;
     }
-    
+
     public void setCode(String code) {
         this.code = code;
     }
-    
+
     public String getSeName() {
         return seName;
     }
-    
+
     public void setSeName(String seName) {
         this.seName = seName;
     }
-    
-    
+
+
     public CourseGroup getCourseGroup() {
         return courseGroup;
     }
-    
+
     public void setCourseGroup(CourseGroup courseGroup) {
         this.courseGroup = courseGroup;
     }
-    
-    
-	   
+
+
+
     public String getNote() {
         return note;
     }
-    
+
     public void setNote(String note) {
         this.note = note;
     }
-    
+
     public Float getCredits() {
         return credits;
     }
-    
+
     public void setCredits(Float credits) {
         this.credits = credits;
     }
  */
- 
-    
+
+
     /* Business methods */
-    
+
     public String getDesignation() {
     	return this.code + " " + this.seName;
     }
-    
+
     public void copyProps (Course c) {
     	this.code = c.getCode();
 		this.seName = c.getSeName();
 		this.courseGroup = c.getCourseGroup();
 		this.note = c.getNote();
 		this.credits = c.getCredits();
-		this.board = c.getBoard();    
+		this.board = c.getBoard();
+		this.inactive = c.isInactive();
     }
 }
